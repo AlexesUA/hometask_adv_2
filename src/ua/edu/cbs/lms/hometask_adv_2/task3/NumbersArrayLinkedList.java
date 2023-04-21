f4057820553c73f61afe4ced4ed9a94a05892a47
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-21 15:55:37 +0300

    Create NumbersArrayLinkedList class

---
+++ src/ua/edu/cbs/lms/hometask_adv_2/task3/NumbersArrayLinkedList.java
@@ -0,0 +1,55 @@
+package ua.edu.cbs.lms.hometask_adv_2.task3;
+
+import ua.edu.cbs.lms.hometask_adv_2.errorshandling.ErrorsHandling;
+
+import java.util.*;
+
+public class NumbersArrayLinkedList {
+    private List<Integer> numbersList = new LinkedList<>();
+
+    public boolean getIntegerList(int count){
+        try{
+            if(count<=0) throw new Exception("Cont can't be equals to or less than 0.");
+
+            Random rndNumber = new Random();
+
+            for(int i=0; i<count; i++){
+                numbersList.add(rndNumber.nextInt(-100, 100));
+            }
+            return true;
+        }catch (Exception error){
+            ErrorsHandling.errorsHandling(error);
+            return false;
+        }
+    }
+
+    public Integer getMinimalNumber(){
+        try {
+            if(!numbersList.iterator().hasNext()) throw new Exception("Arrays is null.");
+
+            //Integer minNumber = numbersList.stream().min((o1, o2) -> o1 - o2).get();
+
+
+            return numbersList.stream().min((o1, o2) -> o1 - o2).get();
+        }catch (Exception error){
+            ErrorsHandling.errorsHandling(error);
+            return null;
+        }
+    }
+
+    public void printArray(){
+        ListIterator<Integer> iteratorNumbers = numbersList.listIterator();
+        int newLineMarker = 30;
+
+        while (iteratorNumbers.hasNext()){
+            System.out.print("[" + iteratorNumbers.next() + "] ");
+            newLineMarker--;
+            if(newLineMarker <= 0){
+                System.out.println();
+                newLineMarker = 30;
+            }
+        }
+
+    }
+
+}
