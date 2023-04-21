23feb15ba0bd7f0e0ed90d085fca2777f7d8934b
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-21 13:27:34 +0300

    Create NumbersArray class

---
+++ src/ua/edu/cbs/lms/hometask_adv_2/task3/NumbersArray.java
@@ -0,0 +1,30 @@
+package ua.edu.cbs.lms.hometask_adv_2.task3;
+
+
+import ua.edu.cbs.lms.hometask_adv_2.errorshandling.ErrorsHandling;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class NumbersArray {
+    Set<Integer> numbersList = new TreeSet<>();
+
+    public boolean getIntegerList(int count){
+        try{
+            Random rndNumber = new Random();
+
+            for(int i=0; i<count; i++){
+                numbersList.add(rndNumber.nextInt(-100, 100));
+            }
+            return true;
+        }catch (Exception error){
+            ErrorsHandling.errorsHandling(error);
+            return false;
+        }
+    }
+
+    public Integer getMinimalNumber(){
+        return numbersList.iterator().next();
+    }
+}
