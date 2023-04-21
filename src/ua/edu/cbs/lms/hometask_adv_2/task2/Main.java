edf495c1129c5153f225fae14fbfdfc867ed913e
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-21 12:32:07 +0300

    Added cycle input words

--- src/ua/edu/cbs/lms/hometask_adv_2/task2/Main.java
+++ src/ua/edu/cbs/lms/hometask_adv_2/task2/Main.java
@@ -1,21 +1,33 @@
 package ua.edu.cbs.lms.hometask_adv_2.task2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Words words = new Words();
+        Scanner scInput = new Scanner(System.in);
 
-        words.addWord("One");
-        words.addWord("Two");
-        words.addWord("Three");
-        words.addWord("Four");
-        words.addWord("Five");
+        System.out.println("Enter 5 words to the keyboard (Split their 'enter' key): ");
 
-        System.out.println("Before double:");
+        int indexWord = 1;
+
+        while (indexWord<6){
+            System.out.print(indexWord + ": ");
+            if(words.addWord(scInput.nextLine())) indexWord++;
+        }
+
+//        words.addWord("One");
+//        words.addWord("Two");
+//        words.addWord("Three");
+//        words.addWord("Four");
+//        words.addWord("Five");
+
+        System.out.println("\nBefore double:");
         words.print();
 
         words.doubleValues();
 
-        System.out.println("After double:");
+        System.out.println("\nAfter double:");
         words.print();
     }
 }
