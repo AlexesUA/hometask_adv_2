423b6c08969fc765751a06748b5c2f766c656341
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-21 11:21:59 +0300

    Created ErrorsHandling class

---
+++ src/ua/edu/cbs/lms/hometask_adv_2/errorshandling/ErrorsHandling.java
@@ -0,0 +1,7 @@
+package ua.edu.cbs.lms.hometask_adv_2.errorshandling;
+
+public class ErrorsHandling {
+    public static void errorsHandling(Exception errors){
+        System.out.println("ERROR: " + errors.getMessage());
+    }
+}
